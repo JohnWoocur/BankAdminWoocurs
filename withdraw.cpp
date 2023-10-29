f161639252ca109cb550bc33f6b451ba9c306ad8
linthu <linthujan99@gmail.com>
2023-10-29 12:40:26 +0530

    withdraw complete by linthu

---
+++ withdraw.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "fileHandler.h"
+#include "withdraw.h"
+
+using namespace std;
+
+int main(){
+    // create_Customer_Account(100,1221212,"linthujan", 99999);
+    withdraw();
+}
